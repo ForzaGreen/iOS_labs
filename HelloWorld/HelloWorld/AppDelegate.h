27c06c5e5a8838230189f2b03c592273e6881fac
Wael <wael.tellat@hotmail.fr>
2015-11-02 22:47:59 +0100

    first commit

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by tellat on 10/20/15.
+//  Copyright (c) 2015 eurecom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
